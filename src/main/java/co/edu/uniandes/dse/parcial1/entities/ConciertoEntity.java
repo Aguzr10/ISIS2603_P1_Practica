e97b0b7888a6d301651cf985248c83cbbae46325
Alejandro <a.guzmanr2@uniandes.edu.co>
2025-09-16 19:17:39 -0500

    Punto 1 Entidades y persistencia

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/ConciertoEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/ConciertoEntity.java
@@ -1,7 +1,9 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
@@ -9,4 +11,12 @@ public class ConciertoEntity extends BaseEntity {
 
     private String nombre;
     private Long presupuesto;
+
+    private String nombreArtista;
+    private String fechaConcierto;
+    private int aforoConcierto;
+
+    @PodamExclude
+    @ManyToOne
+    private EstadioEntity categoria;
 }
